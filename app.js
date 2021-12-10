7b101a92ce5e2b1186da7024817d76f296206fab
Nicholas Hacault <nicholashacaulttabs@gmail.com>
2021-12-09 18:16:30 -0600

    add getStops function

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const streetsEl = document.querySelector('.streets');
 const searchInput = document.forms[0];
 
-// https://api.winnipegtransit.com/v3/streets.json?api-key=7Pc5IOnW-KChC5e61pcR&name=portage
 const apiKey = '7Pc5IOnW-KChC5e61pcR';
 const baseURL = 'https://api.winnipegtransit.com/v3/';
 
+// https://api.winnipegtransit.com/v3/streets.json?api-key=7Pc5IOnW-KChC5e61pcR&name=portage
 const getStreets = (query) => {
   return fetch(`${baseURL}streets.json?api-key=${apiKey}&name=${query}`)
     .then((response) => response.json())
     .then((data) => data.streets);
-}
+};
+
+// https://api.winnipegtransit.com/v3/stops.json?api-key=7Pc5IOnW-KChC5e61pcR&street=2903
+const getStops = (streetKey) => {
+  return fetch(`${baseURL}streets.json?api-key=${apiKey}&street=${streetKey}`)
+    .then((response) => response.json())
+    .then((data) => data.stops);
+};
 
 const handleSearchInput = (e) => {
   e.preventDefault();
@@ -24,7 +31,6 @@ const handleSearchInput = (e) => {
       });
     });
   }
-  
 }
 
 searchInput.addEventListener('submit', handleSearchInput);
